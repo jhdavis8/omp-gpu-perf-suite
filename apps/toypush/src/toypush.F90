ee8dfd9015e8a7a4ec875e443b7244b1acd45b59
Joshua Davis <jhdavis@udel.edu>
2021-03-07 14:02:58 -0500

    Add nvtx module to toypush

--- apps/toypush/src/toypush.F90
+++ apps/toypush/src/toypush.F90
@@ -2,6 +2,58 @@
 !> @author T. Koskela
 !> @date Aug 17 2016
 !<
+
+! Fortran bindings for a small subset of the NVIDIA Tools Extensions library
+! Thanks to Jeff Larkin: https://gist.github.com/jefflarkin/b64f63d79bdc978a2503
+#ifdef NVTX
+module nvtx
+  use iso_c_binding
+  public :: nvtxrangepusha, nvtxrangepop
+  ! public :: nvtxrangepushaargb
+  interface
+     ! Annotate the timeline with a message
+     ! Parameters:
+     ! * string : the message in a string format
+     subroutine nvtxrangepusha(string) bind(C, name="nvtxRangePushA")
+       use iso_c_binding , only : c_char
+       character(kind=c_char) :: string(*)
+     end subroutine nvtxrangepusha
+
+     ! Annotate the timeline with both a message and an ARGB color
+     ! Parameters:
+     ! * string : the message in a string format
+     ! * argb   : the color in argb format (example: Z'FF880000'
+     ! subroutine nvtxrangepushaargb(string,argb) bind(C, name="_nvtxRangePushAARGB")
+     !   use iso_c_binding , only : c_char, c_int
+     !   character(kind=c_char) :: string(*)
+     !   integer(kind=c_int), value  :: argb
+     ! end subroutine nvtxrangepushaargb
+
+     ! Pop the last range off the stack
+     subroutine nvtxrangepop() bind(C, name="nvtxRangePop")
+     end subroutine nvtxrangepop
+
+     ! Place a mark on the timeline with a message
+     ! Parameters:
+     ! * string : the message in a string format
+     ! NOT YET EXPOSED
+     subroutine nvtxMarkA(string) bind(C, name="nvtxMarkA")
+       use iso_c_binding , only : c_char
+       character(kind=c_char) :: string(*)
+     end subroutine nvtxMarkA
+
+     ! Name an OS thread
+     ! NOT YET EXPOSED
+     subroutine nvtxNameOsThread(tid, string) bind(C, name="nvtxNameOsThread")
+       use iso_c_binding , only : c_int, c_char
+       integer(kind=c_int) :: tid
+       character(kind=c_char) :: string(*)
+     end subroutine nvtxNameOsThread
+  end interface
+end module nvtx
+#endif
+
+
 program toypush
 
   use params
@@ -14,6 +66,9 @@ program toypush
 #endif
 #ifdef MPI
   use mpi
+#endif
+#ifdef NVTX
+  use nvtx
 #endif
   
   implicit none  
